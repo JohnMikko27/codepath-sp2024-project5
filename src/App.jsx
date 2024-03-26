1fc92f414c65924b9b71f2440241a0a6c6631915
Mikko <realmiks27@gmail.com>
2024-03-25 17:38:09 -0700

    Add fetchData function

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState(0);
+
+  useEffect(() => {
+    const fetchData = async() => {
+      const myHeaders = new Headers();
+      myHeaders.append("x-rapidapi-key", "891bbdd40e39ebf979dd5dff37b6bb3b");
+      myHeaders.append("x-rapidapi-host", "v2.nba.api-sports.io");
+
+      const requestOptions = {
+        method: "GET",
+        headers: myHeaders,
+        redirect: "follow"
+      };
+
+      const response = await fetch("https://v2.nba.api-sports.io/teams", requestOptions);
+      const data = await response.json();
+      console.log(data);
+      console.log(data.response);
+      setData(data.response);
+    };
+    fetchData();
+  }, []);
 
   return (
-    <div className=" "></div>
+    <div className=" ">hi</div>
   );
 }
 
