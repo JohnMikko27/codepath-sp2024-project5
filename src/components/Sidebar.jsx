9ed297951187c0ab25be3e253d5820d59d2d13c7
Mikko <realmiks27@gmail.com>
2024-03-27 09:53:56 -0700

    Style sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,10 @@
 const Sidebar = () => {
   return (
-    <div>
-      <h1>BballDash</h1>
-      <h2>About</h2>
+    <div className="  flex flex-col gap-8 p-8 ">
+      <h1 className="text-5xl rounded-md hover:underline hover:cursor-pointer">BballDash</h1>
+      <h2 className="text-3xl rounded-md hover:underline hover:cursor-pointer">About</h2>
+      <h2 className="text-3xl rounded-md hover:underline hover:cursor-pointer">Games</h2>
+      <h2 className="text-3xl rounded-md hover:underline hover:cursor-pointer">Contact</h2>
     </div>
   );
 };
