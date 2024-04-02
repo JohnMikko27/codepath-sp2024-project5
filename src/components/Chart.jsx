a4c976325700f3add82a5d89894faa199128754e
Mikko <realmiks27@gmail.com>
2024-04-02 07:24:56 -0700

    Add Chart component

---
+++ src/components/Chart.jsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend
+} from "recharts";
+
+const Chart = ({conference}) => {
+  const [standings, setStandings] = useState();
+
+  useEffect(() => {
+    const fetchData = async() => {  
+      const response = await fetch(`https://v2.nba.api-sports.io/standings?conference=${conference}&league=standard&season=2023`, {
+        "method": "GET",
+        "headers": {
+          "x-rapidapi-host": "v2.nba.api-sports.io",
+          "x-rapidapi-key": "891bbdd40e39ebf979dd5dff37b6bb3b"
+        }});
+      const data = await response.json();
+      console.log(data);
+      const info = data.response.map((team) => ({
+        code: team.team.code,
+        wins: team.win.total,
+        losses: team.loss.total,
+      }));
+      setStandings(info);
+    };
+
+    fetchData();
+
+  }, []);
+
+  return (
+    <BarChart
+      width={400}
+      height={300}
+      data={standings}
+      margin={{
+        top: 5,
+        right: 30,
+        left: 20,
+        bottom: 5
+      }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="code" hide="true"/>
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Bar dataKey="wins" fill="#90EE90" />
+      <Bar dataKey="losses" fill="#ff7f7f" />
+      {console.log(standings)}
+    </BarChart>
+  );
+};
+
+export default Chart;
